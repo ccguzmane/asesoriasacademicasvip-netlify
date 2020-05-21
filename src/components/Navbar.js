2d08d1d45429f7a03ca3af0bf4f6e577a72afd0e
Christian <christian@bluspot.us>
2020-05-21 16:12:22 -0500

    Changed Navbar text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,24 +60,25 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+              
+            </div>
+            <div className="navbar-end has-text-centered">
+            <Link className="navbar-item" to="/about">
+                Nosotros
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Productos
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Contactanos
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
+              {/* <Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
+              </Link> */}
+              {/* <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
                 target="_blank"
@@ -86,7 +87,7 @@ const Navbar = class extends React.Component {
                 <span className="icon">
                   <img src={github} alt="Github" />
                 </span>
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
