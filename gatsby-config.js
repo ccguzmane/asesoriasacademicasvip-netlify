ed7c3118b3cdd98f306ff15aea88f4afb913bb34
Christian <christian@bluspot.us>
2020-05-21 19:30:53 -0500

    Changed site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Asesorías Académicas VIP',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Clases, nivelaciones, trabajos, tesis, traducciones, ediciones, creación de juegos, creación de videojuegos, asesorías educativas, empresariales.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
